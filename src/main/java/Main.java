cfeed61dc4dc3f8f9f29ca950213067e1bda0696
Pedro Vitor Da Cruz Silva <pedrovitordcruzsilva@gmail.com>
2022-05-05 20:34:25 -0400

    add input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 
 public class Main {
 	 public static void main(String[] args) {
 		 HotelReservation hotelReservation = new HotelReservation();
+		 Scanner sc = new Scanner(System.in);
 		 
-		 System.out.println(hotelReservation.getCheapestHotel(""));
+		 String input = sc.nextLine();
+		 
+		 System.out.println(hotelReservation.getCheapestHotel(input));
 	 }
 
 }
